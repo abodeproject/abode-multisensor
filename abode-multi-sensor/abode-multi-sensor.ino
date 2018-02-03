1efae44c1e0e253af538c0a5660ce02a67baa704
Scott Neel <scott@neel.net>
2018-02-03 14:09:56 -0800

    * detect if config exists * use interupt for pir * set off age for motion * general cleanup

--- abode-multi-sensor/abode-multi-sensor.ino
+++ abode-multi-sensor/abode-multi-sensor.ino
@@ -39,17 +39,21 @@ int heartbeatInterval = 30000;
 int lastHeartbeat = 0;
 
 /* Config Vars */
+const int CONFIGVERSION = 1;
 struct Config{
+  int version;
   char name[32];
   char wifi_ssid[32];
   char wifi_pass[32];
   char mqtt_server[32];
-  char mqtt_user[32];
-  char mqtt_pass[32];
+  char mqtt_user[24];
+  char mqtt_pass[24];
   char mqtt_topic[32];
   char ota_pass[24];
   int mqtt_port;
+  int motion_age;
 };
+
 Config cfg;
 
 /* LED Vars */
@@ -75,6 +79,7 @@ int PIRPollInterval = 1000;
 int lastPIRPoll = 0;
 int pirValue;
 int pirLast;
+int pirLastOff = 0;
 
 /* LDR Vars */
 #define LDRPIN A0
@@ -93,11 +98,45 @@ void setup() {
   pinMode(DHTPIN, INPUT);
   pinMode(PIRPIN, INPUT);
   pinMode(LDRPIN, INPUT);
-
+  
   read_config();
   wifi_setup();
+  setup_http();
+  check_mqtt();
   ota_setup();
-  mqtt_setup();
+  
+  pir_setup();
+  dht.begin();
+}
+
+void loop() {
+  server.handleClient();
+  dnsServer.processNextRequest();
+  ArduinoOTA.handle();
+
+  check_mqtt();
+  set_led_status();
+
+  pollDHT();
+  pollPIR();
+  pollLDR();
+
+  process_heartbeat();
+}
+
+void process_heartbeat() {
+  if (millis() < lastHeartbeat) {
+    lastHeartbeat = millis();
+  }
+  
+  if (millis() - lastHeartbeat > heartbeatInterval && connected == 1) {
+    Serial.println("Sending heartbeat.");
+    mqttPublish();
+    lastHeartbeat = millis();
+  }
+}
+
+void setup_http() {
   
   server.on("/", handleRoot);
   server.on("/settings", handleSettings);
@@ -108,16 +147,10 @@ void setup() {
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP server started.");
+  
 }
 
-void loop() {
-  server.handleClient();
-  dnsServer.processNextRequest();
-  ArduinoOTA.handle();
-
-  if (!mqtt.connected()) {
-    mqtt_setup();
-  }
+void set_led_status() {
 
   if (WiFi.status() != WL_CONNECTED) {
     set_led(255, 0, 0);
@@ -126,22 +159,7 @@ void loop() {
   } else {
     set_led(0, 32, 0);
   }
-
-  pollDHT();
-  pollPIR();
-  pollLDR();
-
-  if (millis() < lastHeartbeat) {
-    lastHeartbeat = millis();
-  }
   
-  if (millis() - lastHeartbeat > heartbeatInterval && connected == 1) {
-    Serial.println("Sending heartbeat.");
-    mqttPublish();
-    lastHeartbeat = millis();
-  }
-
-  delay(1000);
 }
 
 void set_led(int red, int green, int blue) {
@@ -185,11 +203,15 @@ void ota_setup() {
   Serial.println("done");
 }
 
-void mqtt_setup() {
+void check_mqtt() {
   if (connected == 0) {
     return;
   }
 
+  if (mqtt.connected()) {
+    return;
+  }
+  
   if (millis() - lastMqttAttempt > mqttAttemptInterval) {
     lastMqttAttempt = millis();
     
@@ -295,25 +317,27 @@ void mqttHandler (char* topic, byte* payload, unsigned int length) {
 void wifi_setup() {
   /* Try to connect to the Wifi */
   WiFi.mode(WIFI_STA);
-  
-  Serial.print("Connecting to WiFi '");
-  Serial.print(cfg.wifi_ssid);
-  Serial.print("'");
-  WiFi.begin(cfg.wifi_ssid, cfg.wifi_pass);
-  
-  int tries = 0;
 
-  /* Wait for connection */
-  while (WiFi.status() != WL_CONNECTED) {
-    tries++;
-
-    if (tries > max_tries) {
-      Serial.println("failed");
-      break;
-    }
+  if (strlen(cfg.wifi_ssid) != 0) {
+    Serial.print("Connecting to WiFi '");
+    Serial.print(cfg.wifi_ssid);
+    Serial.print("'");
+    WiFi.begin(cfg.wifi_ssid, cfg.wifi_pass);
     
-    Serial.print(".");
-    delay(1000);
+    int tries = 0;
+  
+    /* Wait for connection */
+    while (WiFi.status() != WL_CONNECTED) {
+      tries++;
+  
+      if (tries > max_tries) {
+        Serial.println("failed");
+        break;
+      }
+      
+      Serial.print(".");
+      delay(1000);
+    }
   }
 
   if (WiFi.status() != WL_CONNECTED) {
@@ -343,10 +367,39 @@ void read_config() {
   EEPROM.begin(256);
   Serial.println("Reading Config from EEPROM");
   EEPROM.get(0, cfg);
+
+  Serial.print("Configuration Version: ");
+  Serial.println(cfg.version);
+  
+  if (cfg.version != CONFIGVERSION) {
+    Serial.println("No configuration found");
+
+    cfg.version = CONFIGVERSION;
+    Serial.print("Setting configuration version: ");
+    Serial.println(cfg.version);
+    
+    WiFi.macAddress(mac);
+    String("sensor_" + String(mac[3], HEX) + String(mac[4], HEX) + String(mac[5], HEX)).toCharArray(cfg.name, 32);
+    String("").toCharArray(cfg.wifi_ssid, 32);
+    String("").toCharArray(cfg.wifi_pass, 32);
+    String("").toCharArray(cfg.mqtt_server, 32);
+    String("").toCharArray(cfg.mqtt_user, 32);
+    String("").toCharArray(cfg.mqtt_pass, 32);
+    String("").toCharArray(cfg.mqtt_topic, 32);
+    String("").toCharArray(cfg.ota_pass, 24);
+    cfg.mqtt_port = 1883;
+    cfg.motion_age = 60;
+  
+  }
 }
 
 void pollDHT() {
 
+  /* Handle rollover */
+  if (millis() < lastDHTPoll) {
+    lastDHTPoll = millis();
+  }
+  
   if (millis() - lastDHTPoll > DHTPollInterval) {
     
     float newTempValue = dht.readTemperature(true); //to use celsius remove the true text inside the parentheses  
@@ -374,33 +427,51 @@ void pollDHT() {
       Serial.println(humValue);
     }
   }
-
-  /* Handle rollover */
-  if (millis() < lastDHTPoll) {
-    lastDHTPoll = millis();
-  }
   
+}
+
+void pir_setup() {
+  pirValue = digitalRead(PIRPIN);
+  
+  attachInterrupt(digitalPinToInterrupt(PIRPIN), handlePirOn, RISING);
+}
+
+void handlePirOn() {
+  detachInterrupt(PIRPIN);
+  attachInterrupt(digitalPinToInterrupt(PIRPIN), handlePirOff, FALLING);
+  
+  pirValue = HIGH;
+  pirLastOff = 0;
+
+  if (pirValue != pirLast) {
+    Serial.println("Motion detected");
+    mqttPublish();
+    pirLast = pirValue;
+  } else {
+    Serial.println("Resetting motion age");
+  }
+}
+
+void handlePirOff() {
+  detachInterrupt(PIRPIN);
+  attachInterrupt(digitalPinToInterrupt(PIRPIN), handlePirOn, RISING);
+
+  Serial.println("Motion no longer detected, waiting for motion age");
+  pirLastOff = millis();
 }
 
 void pollPIR() {
 
-  if (millis() - lastPIRPoll > PIRPollInterval) {
-    
-    pirValue = digitalRead(PIRPIN);
-    lastPIRPoll = millis();
-
-    if (pirValue != pirLast) {
-      mqttPublish();
-      pirLast = pirValue;
-      
-      Serial.print("Polled PIR: ");
-      Serial.println(pirValue);
-    }
+  if (pirLastOff == 0) {
+    return;
   }
 
-  /* Handle rollover */
-  if (millis() < lastPIRPoll) {
-    lastPIRPoll = millis();
+  
+  if (millis() - pirLastOff > cfg.motion_age * 1000) {
+    Serial.println("Motion age reached, no motion detected");
+    pirValue = LOW;
+    pirLastOff = 0;
+    mqttPublish();
   }
 }
 
@@ -409,6 +480,10 @@ void pollLDR() {
   if (millis() - lastLDRPoll > PLDRPollInterval) {
     
     ldrValue = analogRead(LDRPIN);
+    ldrValue = ldrValue / 1024 * 100;
+    if (ldrValue > 100) {
+      ldrValue = 100;
+    }
     lastLDRPoll = millis();
 
     if (ldrValue != ldrLast) {
@@ -444,10 +519,11 @@ String toStringIp(IPAddress ip) {
   res += String(((ip >> 8 * 3)) & 0xFF);
   return res;
 }
+
 boolean captivePortal() {
    if (!isIp(server.hostHeader())) {
-    Serial.print("Request redirected to captive portal");
-    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+    Serial.println("Request redirected to captive portal");
+    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()) + "/settings", true);
     server.send ( 302, "text/plain", ""); 
     server.client().stop();
     return true;
@@ -474,7 +550,7 @@ const String styles = "<style>"
 "#content {"
   "margin: 20px auto;"
   "background-color: #272b30;"
-  "width: 400px;"
+  "max-width: 400px;"
   "border: 1px solid black;"
   "border-radius: 4px 4px;"
   "padding: 10px;"
@@ -530,6 +606,15 @@ const String styles = "<style>"
 ".signal {"
   "float: right;"
   "font-size: .8em;"
+"}"
+".label {"
+  "font-weight: bold;"
+"}"
+"input {"
+  "margin: 2px;"
+  "padding: 4px;"
+  "border: 1px solid gray;"
+  "border-radius: 6px 6px;"
 "}"
 "</style>";
 
@@ -597,7 +682,7 @@ void handleSettings() {
           "e.className = (n.e) ? 'encrypted' : '';\n"
           "e.innerHTML += '<span class=\"encryption\">&#9919;</span>' + n.n + '<span class=\"signal\">' + n.s + '</span>';\n"
           "e.onclick=function() {\n"
-          "document.forms.config.ssid.value=n.n;\n"
+          "document.forms.config.wifi_ssid.value=n.n;\n"
           "};\n"
           "document.getElementById('networks').appendChild(e);\n"
           "});\n"
@@ -619,18 +704,27 @@ void handleSettings() {
   "<li><a href='/restart'>Restart</a></li>"
   "</ul>"
   "<form name='config' action='/save' method='post'>"
-  "<h4>General</h4>"
+  "<h3>General</h3>"
+  "<div class='label'>Sensor Name</div>"
   "<div><input type='input' placeholder='Name' name='name' value='" + cfg.name + "'></div>"
-  "<div><input type='input' placeholder='OTA Password' name='ota_password' value='" + cfg.ota_pass + "'></div>"
-  "<h4>MQTT Settings</h4>"
-  "<div><input type='input' placeholder='MQTT Server' name='mqtt_server' value='" + cfg.mqtt_server + "'></div>"
-  "<div><input type='input' placeholder='MQTT User' name='mqtt_user' value='" + cfg.mqtt_user + "'></div>"
-  "<div><input type='input' placeholder='MQTT Password' name='mqtt_password'></div>"
-  "<div><input type='input' placeholder='MQTT Topic' name='mqtt_topic' value='" + cfg.mqtt_topic + "'></div>"
+  "<div class='label'>OTA Password</div>"
+  "<div><input type='input' placeholder='OTA Password' name='ota_password' autocapitalize='off' value='" + cfg.ota_pass + "'></div>"
+  "<div class='label'>Motion Age (seconds)</div>"
+  "<div><input type='input' placeholder='Motion Age (seconds)' name='motion_age' value='" + cfg.motion_age + "'></div>"
+  "<h3>MQTT Settings</h3>"
+  "<div class='label'>Server</div>"
+  "<div><input type='input' placeholder='MQTT Server' name='mqtt_server' autocapitalize='off' value='" + cfg.mqtt_server + "'></div>"
+  "<div class='label'>User</div>"
+  "<div><input type='input' placeholder='MQTT User' name='mqtt_user' autocapitalize='off' value='" + cfg.mqtt_user + "'></div>"
+  "<div class='label'>Password</div>"
+  "<div><input type='input' placeholder='MQTT Password' name='mqtt_password'autocapitalize='off' ></div>"
+  "<div class='label'>Topic</div>"
+  "<div><input type='input' placeholder='MQTT Topic' name='mqtt_topic' autocapitalize='off' value='" + cfg.mqtt_topic + "'></div>"
+  "<div class='label'>Port</div>"
   "<div><input type='input' placeholder='MQTT Port' name='mqtt_port' value='" + cfg.mqtt_port + "'></div>"
-  "<h4>WiFi Settings</h4>"
+  "<h3>WiFi Settings</h3>"
   "<div>&nbsp;</div>"
-  "<button onclick='getNetworks()'>Scan Networks</button>"
+  "<button type='button' onclick='getNetworks()'>Scan Networks</button>"
   "<div>&nbsp;</div>"
   "<div id='i'></div>"
   "<div id='e'></div>"
@@ -638,7 +732,9 @@ void handleSettings() {
     "<ul id='networks'></ul>"
   "</div>"
   "<div>&nbsp;</div>"
+  "<div class='label'>SSID</div>"
   "<div><input type='input' placeholder='SSID' name='wifi_ssid' value='" + cfg.wifi_ssid + "'></div>"
+  "<div class='label'>Password</div>"
   "<div><input type='input' placeholder='Password' name='wifi_password'></div>"
   "<div>&nbsp;</div>"
   "<div><input type='submit' value='Save'></div>"
@@ -676,6 +772,7 @@ void returnFail(String msg) {
 void handleSave() {
 
   int changed = 0;
+  int reboot = 0;
 
   if (server.hasArg("name")) {
     changed = 1;
@@ -687,6 +784,7 @@ void handleSave() {
 
   if (server.hasArg("ota_password") && server.arg("ota_password") != "") {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving OTA Password");
     char ota_pass_char[24];
     server.arg("ota_password").toCharArray(ota_pass_char, 24);
@@ -695,6 +793,7 @@ void handleSave() {
 
   if (server.hasArg("mqtt_server")) {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving MQTT Server");
     char mqtt_server_char[32];
     server.arg("mqtt_server").toCharArray(mqtt_server_char, 32);
@@ -703,6 +802,7 @@ void handleSave() {
 
   if (server.hasArg("mqtt_user")) {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving MQTT User");
     char mqtt_user_char[32];
     server.arg("mqtt_user").toCharArray(mqtt_user_char, 32);
@@ -711,6 +811,7 @@ void handleSave() {
 
   if (server.hasArg("mqtt_password") && server.arg("mqtt_password") != "") {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving MQTT Password");
     char mqtt_password_char[32];
     server.arg("mqtt_password").toCharArray(mqtt_password_char, 32);
@@ -719,12 +820,14 @@ void handleSave() {
 
   if (server.hasArg("mqtt_port")) {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving MQTT Port");
     cfg.mqtt_port = server.arg("mqtt_port").toInt();
   }
 
   if (server.hasArg("mqtt_topic")) {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving MQTT Topic");
     char mqtt_topic_char[32];
     server.arg("mqtt_topic").toCharArray(mqtt_topic_char, 32);
@@ -733,6 +836,7 @@ void handleSave() {
 
   if (server.hasArg("wifi_ssid")) {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving Wifi SSID");
     char wifi_ssid_char[32];
     server.arg("wifi_ssid").toCharArray(wifi_ssid_char, 32);
@@ -741,10 +845,17 @@ void handleSave() {
 
   if (server.hasArg("wifi_password") && server.arg("wifi_password") != "") {
     changed = 1;
+    reboot = 1;
     Serial.println("Saving WIFI Password");
     char wifi_password_char[32];
     server.arg("wifi_password").toCharArray(wifi_password_char, 32);
     strcpy(cfg.wifi_pass, wifi_password_char);
+  }
+
+  if (server.hasArg("motion_age")) {
+    changed = 1;
+    Serial.println("Saving Motion Age");
+    cfg.motion_age = server.arg("motion_age").toInt();
   }
 
   if (changed == 0) {
@@ -756,7 +867,6 @@ void handleSave() {
   EEPROM.begin(256);
   EEPROM.put(0, cfg);
   EEPROM.commit();
-  delay(1000);
   
   String document = ""
   "<html>"
@@ -767,15 +877,17 @@ void handleSave() {
   "" + styles + ""
   "</head>"
   "<body>"
-  "<div id='content'>Applying configuration. Device will reboot and attempt to connect to wifi.</div>"
+  "<div id='content'>Applying configuration. Device may reboot if WiFi or MQTT settings changed.</div>"
   "</body>"
   "</html>";
   
   server.send(200, "text/html", document);
-  Serial.println("Restarting...");
-  delay(2000);
-
-  WiFi.forceSleepBegin(); wdt_reset(); ESP.restart(); while(1)wdt_reset();
+  if (reboot) {
+    Serial.println("Restarting...");
+    delay(2000);
+  
+    WiFi.forceSleepBegin(); wdt_reset(); ESP.restart(); while(1)wdt_reset();
+  }
 }
 
 void handleRestart() {
